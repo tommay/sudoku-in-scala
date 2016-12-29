8b02dbf9b9572ca3adc01a10fae93f6b7db8c480
Tom May <tom@tommay.net>
2016-12-28 17:45:59 -0800

    Start on the haskell -> scala translation.

---
+++ net/tommay/sudoku/Util.scala
@@ -0,0 +1,13 @@
+package net.tommay.sudoku
+
+class Util {
+}
+
+object Util {
+  // Slice an list up into sub-lists of n elements, and return the
+  // sub-lists in a list.
+
+  def slices[T](n: Int, list: Seq[T]) : List[Seq[T]] = {
+    list.grouped(n).toList
+  }
+}
