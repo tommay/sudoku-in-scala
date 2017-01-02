5ccfd3e05c99c4157b16f4ca95f7934ee40883d3
Tom May <tom@tommay.net>
2017-01-02 02:27:46 -0800

    Util.slices uses Iterable, it's more general.

--- src/main/scala/net/tommay/sudoku/Util.scala
+++ src/main/scala/net/tommay/sudoku/Util.scala
@@ -4,7 +4,7 @@ object Util {
   // Slice an list up into sub-lists of n elements, and return the
   // sub-lists in a list.
 
-  def slices[T](n: Int, list: Seq[T]) : List[Seq[T]] = {
+  def slices[T](n: Int, list: Iterable[T]) : List[Iterable[T]] = {
     list.grouped(n).toList
   }
 
