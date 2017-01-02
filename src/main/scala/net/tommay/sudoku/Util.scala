dd96b843f11c9ba6afa0db39017edd44070efe1b
Tom May <tom@tommay.net>
2017-01-01 23:29:31 -0800

    Use List#minBy instead of rolling our own.

--- src/main/scala/net/tommay/sudoku/Util.scala
+++ src/main/scala/net/tommay/sudoku/Util.scala
@@ -8,19 +8,6 @@ object Util {
     list.grouped(n).toList
   }
 
-  // Weird generic voodoo as per
-  // http://stackoverflow.com/questions/19385235/how-to-paramaterize-int-as-ordered-in-scala
-  //
-  def minBy[T, K <% Ordered[K]](list: Iterable[T], func: T => K) : T = {
-    val enhanced = list.map(e => (func(e), e))
-    val minEnhanced = enhanced.tail.foldLeft(enhanced.head) {
-      case (a@(na, _), b@(nb, _)) =>
-        // xxx choose a or b on ==?
-        if (na < nb) a else b
-    }
-    minEnhanced._2
-  }
-
   def shuffle[T](list: List[T], rnd: Random) : List[T] = {
     // XXX
     list
